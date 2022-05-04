1d28bf1678e87095c8c7b459c3c320cbdd730d4c
PatrickStarM <625790276@qq.com>
2022-05-04 23:23:44 +0800

    feat: 用户被动退出方案之主动处理

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,6 +5,7 @@ import { setItem, getItem, removeAllItem } from '@/utils/storage'
 import { TOKEN } from '@/constant'
 // 增加 导入路由
 import router from '@/router'
+import { setTimeStamp } from '@/utils/auth'
 export default {
   namespaced: true,
   state: () => ({
@@ -30,9 +31,9 @@ export default {
           password: md5(password)
         })
           .then((data) => {
+            setTimeStamp()
             resolve(data)
             console.log(data)
-            // this.commit('user/setToken', data.data.data.token)
             this.commit('user/setToken', data.token)
           })
           .catch((err) => {
