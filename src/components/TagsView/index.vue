c293b09f7ae6644401afbcfc2d5960d857063336
PatrickStarM <625790276@qq.com>
2022-05-06 20:18:14 +0800

    feat: contextMenu 事件处理

--- src/components/TagsView/index.vue
+++ src/components/TagsView/index.vue
@@ -24,7 +24,9 @@
 <script setup>
 import { useRoute } from 'vue-router'
 import ContextMenu from './ContextMenu.vue'
-import { ref, reactive } from 'vue'
+import { ref, reactive, watch } from 'vue'
+import { useStore } from 'vuex'
+
 const route = useRoute()
 
 /**
@@ -33,11 +35,6 @@ const route = useRoute()
 const isActive = (tag) => {
   return tag.path === route.path
 }
-
-/**
- * 关闭 tag 的点击事件
- */
-const onCloseClick = (index) => {}
 
 // contextMenu 相关
 const selectIndex = ref(0)
@@ -56,6 +53,35 @@ const openMenu = (e, index) => {
   selectIndex.value = index
   visible.value = true
 }
+
+/**
+ * 关闭 tag 的点击事件
+ */
+const store = useStore()
+const onCloseClick = (index) => {
+  store.commit('app/removeTagsView', {
+    type: 'index',
+    index: index
+  })
+}
+
+/**
+ * 关闭 右键 menu
+ */
+const closeMenu = () => {
+  visible.value = false
+}
+
+/**
+ * 监听变化
+ */
+watch(visible, (val) => {
+  if (val) {
+    document.body.addEventListener('click', closeMenu)
+  } else {
+    document.body.removeEventListener('click', closeMenu)
+  }
+})
 </script>
 
 <style lang="scss" scoped>
