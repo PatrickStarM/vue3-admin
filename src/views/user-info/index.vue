f5221a82b6f50db42dc711ac3d5382a972a44f79
PatrickStarM <625790276@qq.com>
2022-05-07 21:05:52 +0800

    feat: 获取展示数据

--- src/views/user-info/index.vue
+++ src/views/user-info/index.vue
@@ -2,8 +2,26 @@
   <div>user-info</div>
 </template>
 
-<script>
-export default {}
+<script setup>
+import { userDetail } from '@/api/user-manage'
+import { watchSwitchLang } from '@/utils/i18n'
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  }
+})
+
+// 数据相关
+const detailData = ref({})
+const getUserDetail = async () => {
+  detailData.value = await userDetail(props.id)
+}
+getUserDetail()
+// 语言切换
+watchSwitchLang(getUserDetail)
 </script>
 
 <style></style>
