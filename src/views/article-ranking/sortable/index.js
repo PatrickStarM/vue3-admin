d6aaa8fda2aa9dffb641ece1f31a263aa736e270
PatrickStarM <625790276@qq.com>
2022-05-09 19:05:10 +0800

    feat: 完成拖拽后的排序

--- src/views/article-ranking/sortable/index.js
+++ src/views/article-ranking/sortable/index.js
@@ -2,6 +2,7 @@ import { ref } from 'vue'
 import Sortable from 'sortablejs'
 import i18n from '@/i18n'
 import { ElMessage } from 'element-plus'
+import { articleSort } from '@/api/article'
 
 // 排序相关
 export const tableRef = ref(null)
@@ -21,7 +22,11 @@ export const initSortable = (tableData, cb) => {
     async onEnd(event) {
       const { newIndex, oldIndex } = event
       // 修改数据
-      alert('拖拽完毕', newIndex, oldIndex)
+      alert(tableData.value[oldIndex].ranking + ',' + tableData.value[newIndex].ranking)
+      await articleSort({
+        initRanking: tableData.value[oldIndex].ranking,
+        finalRanking: tableData.value[newIndex].ranking
+      })
       ElMessage.success({
         message: i18n.global.t('msg.article.sortSuccess'),
         type: 'success'
